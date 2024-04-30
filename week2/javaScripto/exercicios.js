e19dc6de65bc8ac0f47134c1d8efe45fdefc82b6
Lucas Eifler <lucass.eifler@gmail.comm>
2024-04-30 21:07:14 +0200

    Recap basics of JS.

---
+++ week2/javaScripto/exercicios.js
@@ -0,0 +1,11 @@
+/* exercício 1 
+criar 3 variáveis (strings) e colocar no console;
+
+exercício 2 
+criar 3 variáveis (números) e colocar no console cada uma das variáveis;
+
+exercício 3 
+criar 3 objetos e colocar no console;
+
+exercício 4 
+criar 1 array(com 6 elementos dentro do array) e colocar no console; */
---
+++ week2/javaScripto/index.js
@@ -0,0 +1,29 @@
+/* const n1 = prompt("digite 1 número");
+
+console.log(n1);
+ */
+// const = nunca muda
+// let =
+// var = não usar mais - ES5 ----> ES6
+
+// lógica de programação
+// linguagem de programação
+
+const user1 = {
+  first_name: "Nome1",
+  last_name: "Sobrenome1",
+  email: "email@example.com1",
+};
+
+const user2 = {
+  first_name: "Nome2",
+  last_name: "Sobrenome2",
+  email: "email@example.com2",
+};
+
+/* console.log(user1);
+console.log(user2);
+ */
+const arr = [user1, user2, 4, 5, 6, 7, "lucas é lindo"];
+
+/* console.log(arr[4]); */
